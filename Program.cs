fc5b3bfe3c820ceba0367eee6b8ae3daca19b613
Aleksandra Nizio <ola.nizio@o2.pl>
2024-11-20 11:33:19 +0100

    Dodawanie póki co

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace Filtr_czarno_biały
 {
     internal class Program
     {
+        [DllImport(@"C:\Users\olani\source\repos\Filtr czarno-biały\x64\Debug\ASM.dll")]
+        static extern int MyProc1(int a, int b);
         static void Main(string[] args)
         {
+            int x = 5, y = 3;
+            int ret = MyProc1(x, y);
+            Console.Write("Moja pierwsza wartość obliczona w asm to:");
+            Console.WriteLine(ret);
+            Console.ReadLine();
         }
     }
 }
