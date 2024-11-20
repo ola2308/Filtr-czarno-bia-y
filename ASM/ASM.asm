f3a0084bfbde5809c3e09985ead5a4ace7a91bce
Aleksandra Nizio <ola.nizio@o2.pl>
2024-11-20 13:07:41 +0100

    Coś tam działa nawet ładnie, zmiany w C# jeszcze, asm się kompiluje, nie wyrzuca już błędów. zdjęcie zmienia się na czarno białe, ale narazie bez parametryzacji, standardowo, muszę dodać parametryzacje :(

--- ASM/ASM.asm
+++ ASM/ASM.asm
@@ -1,7 +1,85 @@
+.data
+    align 16
+    rgb_weights REAL4 0.299, 0.587, 0.114, 0.0
+    mask_blue DD 0FFh, 0FFh, 0FFh, 0FFh
+    align 16
+
 .code
-MyProc1 proc
-add RCX, RDX
-mov RAX, RCX
-ret
-MyProc1 endp
-end
+GrayscaleFilter PROC
+    ; Function prologue
+    push rbp
+    mov rbp, rsp
+    
+    ; Preserve non-volatile registers
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+
+    ; Parameters:
+    ; rcx = input buffer
+    ; rdx = output buffer
+    ; r8 = pixel count
+    
+    ; Store parameters in non-volatile registers
+    mov r12, rcx    ; Input buffer
+    mov r13, rdx    ; Output buffer
+    mov r14, r8     ; Pixel count
+    
+    ; Load RGB weights into xmm15 (non-volatile)
+    movups xmm15, rgb_weights
+
+pixel_loop:
+    ; Check if we've processed all pixels
+    test r14, r14
+    jz ending
+    
+    ; Load 3 bytes (1 pixel) from input
+    movzx eax, BYTE PTR [r12]      ; Blue
+    movzx ebx, BYTE PTR [r12 + 1]  ; Green
+    movzx ecx, BYTE PTR [r12 + 2]  ; Red
+    
+    ; Convert to float
+    cvtsi2ss xmm0, eax
+    cvtsi2ss xmm1, ebx
+    cvtsi2ss xmm2, ecx
+    
+    ; Multiply by weights
+    mulss xmm0, xmm15              ; Blue * 0.114
+    mulss xmm1, DWORD PTR [rgb_weights + 4]  ; Green * 0.587
+    mulss xmm2, DWORD PTR [rgb_weights + 8]  ; Red * 0.299
+    
+    ; Sum the weighted values
+    addss xmm0, xmm1
+    addss xmm0, xmm2
+    
+    ; Convert back to integer
+    cvtss2si eax, xmm0
+    
+    ; Write the same value to all three channels
+    mov BYTE PTR [r13], al
+    mov BYTE PTR [r13 + 1], al
+    mov BYTE PTR [r13 + 2], al
+    
+    ; Move to next pixel
+    add r12, 3      ; Input buffer (RGB = 3 bytes)
+    add r13, 3      ; Output buffer
+    dec r14         ; Decrement pixel count
+    jmp pixel_loop
+
+ending:
+    ; Restore non-volatile registers
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    
+    ; Function epilogue
+    mov rsp, rbp
+    pop rbp
+    ret
+GrayscaleFilter ENDP
+
+END
--- Program.cs
+++ Program.cs
@@ -1,23 +1,158 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Filtr_czarno_biały
+namespace FiltrCzarnoBialy
 {
-    internal class Program
+    public class Program
     {
         [DllImport(@"C:\Users\olani\source\repos\Filtr czarno-biały\x64\Debug\ASM.dll")]
-        static extern int MyProc1(int a, int b);
-        static void Main(string[] args)
+        public static extern void GrayscaleFilter(IntPtr inputPixels, IntPtr outputPixels, int pixelCount);
+
+        [STAThread]
+        static void Main()
         {
-            int x = 5, y = 3;
-            int ret = MyProc1(x, y);
-            Console.Write("Moja pierwsza wartość obliczona w asm to:");
-            Console.WriteLine(ret);
-            Console.ReadLine();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    public class MainForm : Form
+    {
+        private PictureBox originalPictureBox;
+        private PictureBox processedPictureBox;
+        private Label instructionLabel;
+
+        public MainForm()
+        {
+            // Ustawienia okna
+            this.Text = "Filtr Czarno-Biały";
+            this.Width = 1000;
+            this.Height = 600;
+            this.AllowDrop = true; // Włącz obsługę przeciągania i upuszczania
+
+            // Instrukcja
+            instructionLabel = new Label
+            {
+                Text = "Przeciągnij i upuść obraz tutaj.",
+                Dock = DockStyle.Top,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Arial", 12)
+            };
+            this.Controls.Add(instructionLabel);
+
+            // Pole do wyświetlania oryginalnego obrazu
+            originalPictureBox = new PictureBox
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Width = this.Width / 2 - 20,
+                Height = this.Height - 100,
+                Left = 10,
+                Top = 50,
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+            this.Controls.Add(originalPictureBox);
+
+            // Pole do wyświetlania przetworzonego obrazu
+            processedPictureBox = new PictureBox
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Width = this.Width / 2 - 20,
+                Height = this.Height - 100,
+                Left = this.Width / 2 + 10,
+                Top = 50,
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+            this.Controls.Add(processedPictureBox);
+
+            // Obsługa przeciągania i upuszczania
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0)
+            {
+                string inputPath = files[0];
+                string outputPath = System.IO.Path.Combine(
+                    System.IO.Path.GetDirectoryName(inputPath),
+                    "output.jpg"
+                );
+
+                try
+                {
+                    // Wyświetl oryginalny obraz
+                    originalPictureBox.Image = Image.FromFile(inputPath);
+
+                    // Przetwórz obraz
+                    RunGrayscaleFilter(inputPath, outputPath);
+
+                    // Wyświetl przetworzony obraz
+                    processedPictureBox.Image = Image.FromFile(outputPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void RunGrayscaleFilter(string inputPath, string outputPath)
+        {
+            using (Bitmap inputBitmap = new Bitmap(inputPath))
+            using (Bitmap outputBitmap = new Bitmap(inputBitmap.Width, inputBitmap.Height, PixelFormat.Format24bppRgb))
+            {
+                Rectangle rect = new Rectangle(0, 0, inputBitmap.Width, inputBitmap.Height);
+                BitmapData inputData = inputBitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+                int pixelCount = inputBitmap.Width * inputBitmap.Height;
+                int stride = inputData.Stride;
+
+                IntPtr inputPixels = Marshal.AllocHGlobal(stride * inputBitmap.Height);
+                IntPtr outputPixels = Marshal.AllocHGlobal(stride * inputBitmap.Height);
+
+                try
+                {
+                    byte[] inputArray = new byte[stride * inputBitmap.Height];
+                    Marshal.Copy(inputData.Scan0, inputArray, 0, inputArray.Length);
+                    Marshal.Copy(inputArray, 0, inputPixels, inputArray.Length);
+                    inputBitmap.UnlockBits(inputData);
+
+                    // Wywołaj funkcję DLL
+                    Program.GrayscaleFilter(inputPixels, outputPixels, pixelCount);
+
+                    BitmapData outputData = outputBitmap.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+                    byte[] outputArray = new byte[stride * outputBitmap.Height];
+                    Marshal.Copy(outputPixels, outputArray, 0, outputArray.Length);
+                    Marshal.Copy(outputArray, 0, outputData.Scan0, outputArray.Length);
+                    outputBitmap.UnlockBits(outputData);
+
+                    outputBitmap.Save(outputPath, ImageFormat.Jpeg);
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(inputPixels);
+                    Marshal.FreeHGlobal(outputPixels);
+                }
+            }
         }
     }
 }
